e873baf0d0c2724fec138317d7ea11e1f7de24e4
h0x0er <jatink843@protonmail.com>
2024-08-01 13:16:32 +0530

    added approval-ci hook

---
+++ images/hooks/job-started.d/approval-hook.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+set -u
+source logger.sh
+
+step-log-debug () { log.debug "[StepSecurity] $1"; }
+step-log-error () { log.error "[StepSecurity] $1"; }
+step-log-success  () { log.success "[StepSecurity] $1"; }
+step-log-warning  () { log.warning "[StepSecurity] $1"; }
+step-log-notice  () { log.notice "[StepSecurity] $1"; }
+
+
+
+GITHUB_REPOSITORY=${GITHUB_REPOSITORY:-}
+GITHUB_RUN_ID=${GITHUB_RUN_ID:-}
+GITHUB_SHA=${GITHUB_SHA:-}
+
+
+api_base="https://int.api.stepsecurity.io/v1"
+should_ci_run="$api_base/github/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID/should-ci-run"
+
+
+
+function handleResponse(){
+    local resp=${1}
+    local lastStatus=${2}
+
+    local err
+    echo "$resp" | grep -q "error"  > /dev/null
+    err=$?
+    if [[ $err -eq 0 ]] || [[ $lastStatus -ne 0 ]]; then
+        step-log-error "error response received: %s \nExit 1\n" "$resp"
+        exit 1
+    fi
+    
+
+    local isApproved
+    echo "$resp" | grep -q "approved_by" > /dev/null
+    isApproved=$?
+    if [[ $isApproved -eq 0 ]]; then
+
+        approver=$(echo "$resp" | jq '.approved_by')
+        step-log-success "approved by: %s\n" "$approver"
+        exit 0
+
+    fi
+
+}
+
+function printApprovalInfo(){
+
+    step-log-notice "approval_url: https://int1.stepsecurity.io/github/$GITHUB_REPOSITORY/commits/$GITHUB_SHA/approve-ci-run \n"
+
+    step-log-warning "waiting to be approved..\n"
+
+}
+
+
+function main(){
+
+    local resp
+    local counter
+    local maxWait
+
+    counter=0
+    maxWait=60 # wait for 5 minutes
+    
+    printApprovalInfo
+
+    while [[ $counter -ne $maxWait ]]; do
+        # step-log-debug "[$counter] waiting.."
+
+        resp=$(curl -XGET -s "${should_ci_run}")
+        handleResponse "${resp}" $?
+
+        counter=$((counter += 1))
+        sleep 5
+
+    done
+
+    step-log-warning "no-one approved run, waited for maximum time, exit 1\n"
+    exit 1
+
+}
+
+
+main
+
+
+
+
+
+
+
+
+
